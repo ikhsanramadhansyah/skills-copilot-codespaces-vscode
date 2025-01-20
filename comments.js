21303c8d6647a6dca11357a906bc5f18154417e8
ikhsanramadhansyah <139298746+ikhsanramadhansyah@users.noreply.github.com>
2025-01-20 07:29:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,28 @@
+// Create web server
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Create a comment
+app.post('/comments', (req, res) => {
+  res.send('Create a comment');
+});
+
+// Read a comment
+app.get('/comments/:id', (req, res) => {
+  res.send('Read a comment');
+});
+
+// Update a comment
+app.put('/comments/:id', (req, res) => {
+  res.send('Update a comment');
+});
+
+// Delete a comment
+app.delete('/comments/:id', (req, res) => {
+  res.send('Delete a comment');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
